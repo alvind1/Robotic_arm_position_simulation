56ca816b056f69cf57cd15afe0126af01d9a6caf
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-07-04 14:50:50 -0400

    Update main.m

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,10 +1,16 @@
 grid on;
 
-x = 8;
-y = 2;
-z = 7;
+xmin = 0;
+xmax = 12;
+ymin = -6;
+ymax = 6;
+zmin = 0;
+zmax = 12;
 
-theta_x = pi/4;
+x = 11;
+y = -1.6023;
+z = 6.1969;
+theta_x = pi/10;
 theta_y = pi/6;
 
 z1 = y*tan(theta_x);
@@ -14,15 +20,15 @@ arms_lengths = containers.Map();
 arms_lengths('AB') = z0;
 arms_lengths('BC') = 3;
 arms_lengths('CD') = 4;
-arms_lengths('DE') = 4;
-arms_lengths('EF') = 4;
+arms_lengths('DE') = 5;
+arms_lengths('EF') = 6;
 
 points = containers.Map();
 points('A') = [0, 0, 0];
 points('B') = [0, 0, z0];
 points('C') = [arms_lengths('BC'), 0, z0];
 points('D') = [-1, -1, -1];
-points('E') = [x-cos(theta_y)*arms_lengths('CD'), sin(theta_y)*arms_lengths('CD')*sin(theta_x)+y, sin(theta_y)*arms_lengths('CD')*sin(theta_x)+z];
+points('E') = [x-cos(theta_y)*arms_lengths('EF'), y+sin(theta_y)*arms_lengths('EF')*cos(theta_x), z+sin(theta_y)*arms_lengths('EF')*sin(theta_x)];
 points('F') = [x, y, z];
 
 arms_lengths('CE') = norm(points('E')-points('C'));
@@ -44,15 +50,19 @@ vectors('cross2') = cross(vectors('CE'), vectors('cross1'));
 height = 2*heron(arms_lengths('CD'), arms_lengths('DE'), arms_lengths('CE'))/arms_lengths('CE');
 
 vectors('CG') = sqrt(arms_lengths('CD')^2-height^2)*vectors('CE')/norm(vectors('CE'));
+if sqrt(arms_lengths('DE')^2-height^2) >= norm(points('E')-points('C'))
+    vectors('CG') = -vectors('CG');
+end 
+
 vectors('GD') = height*vectors('cross2')/norm(vectors('cross2'));
 vectors('CD') = vectors('CG')+vectors('GD');
 
 points('D') = points('C')+vectors('CD');
 
 angles = containers.Map();
-angles('C') = cosine_law_angle(norm(points('C')-points('B')), arms_lengths('CD'), norm(points('D')-points('B')));
-angles('D') = cosine_law_angle(arms_lengths('CD'), arms_lengths('DE'), norm(points('E')-points('C')));
-angles('E') = cosine_law_angle(arms_lengths('DE'), arms_lengths('EF'), norm(points('F')-points('D')));
+angles('C') = pi-cosine_law_angle(norm(points('C')-points('B')), arms_lengths('CD'), norm(points('D')-points('B')));
+angles('D') = pi-cosine_law_angle(arms_lengths('CD'), arms_lengths('DE'), norm(points('E')-points('C')));
+angles('E') = pi-cosine_law_angle(arms_lengths('DE'), arms_lengths('EF'), norm(points('F')-points('D')));
 temp = points('D');
 angles('T') = atan((temp(3)-z0)/temp(2));
 
@@ -75,7 +85,7 @@ for i = 1:length(points)
     end
     
     if(i ~= 1 && i ~= 2 && i ~= 6)
-        text(val{i}(1)+0.5, val{i}(2)+0.5, val{i}(3)+0.5, num2str(angles(k{i})));
+        text(val{i}(1)+0.5, val{i}(2)+0.5, val{i}(3)+0.5, num2str(angles(k{i})), 'Color', 'r');
     end 
     
     if (i == 3)
@@ -87,4 +97,4 @@ for i = 1:length(points)
 end
 
 plot3(x_val, y_val, z_val);
-axis([0, 10, -5, 5, 0, 10]);
+axis([xmin, xmax, ymin, ymax, zmin, zmax]);
