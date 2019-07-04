770d8b5d328abcde778a429f3bc5f7c9e909d3c7
alvind1 <40304595+alvind1@users.noreply.github.com>
2019-07-04 14:50:17 -0400

    Update animate.m

--- Matlab/animate.m
+++ Matlab/animate.m
@@ -5,30 +5,41 @@ ylim([-5, 5]);
 zlim = ([0, 10]);
 axis manual;
 
-theta_x = pi/4;
-theta_y = pi/6;
+xmin = 0;
+xmax = 12;
+ymin = -6;
+ymax = 6;
+zmin = 0;
+zmax = 12;
+
+theta_y = pi/8;
+
 %%
 for j = 1:10000
-    [x, y, z] = get_points(j/10);
+    r = 1;
+    xdist = 11;
+    zdist = 3;
     
-    z1 = y*tan(theta_x);
+    [x, y, z, theta_x] = get_points(j/10, r, xdist, zdist);
+    
+    z1 = abs(y)*tan(theta_x); %Weird jump 
     z0 = z-z1;
     
     arms_lengths = containers.Map();
     arms_lengths('AB') = z0;
     arms_lengths('BC') = 3;
     arms_lengths('CD') = 4;
-    arms_lengths('DE') = 4;
-    arms_lengths('EF') = 4;
+    arms_lengths('DE') = 5;
+    arms_lengths('EF') = 6;
     
     points = containers.Map();
     points('A') = [0, 0, 0];
     points('B') = [0, 0, z0];
     points('C') = [arms_lengths('BC'), 0, z0];
     points('D') = [-1, -1, -1];
-    points('E') = [x-cos(theta_y)*arms_lengths('CD'), sin(theta_y)*arms_lengths('CD')*sin(theta_x)+y, sin(theta_y)*arms_lengths('CD')*sin(theta_x)+z];
+    points('E') = [x-cos(theta_y)*arms_lengths('EF'), y+sin(theta_y)*arms_lengths('EF')*cos(theta_x), z+sin(theta_y)*arms_lengths('EF')*sin(theta_x)];
     points('F') = [x, y, z];
-
+    
     arms_lengths('CE') = norm(points('E')-points('C'));
 
     if(norm(points('F')-points('C')) > arms_lengths('CD')+arms_lengths('DE')+arms_lengths('EF'))
@@ -48,8 +59,12 @@ for j = 1:10000
     height = 2*heron(arms_lengths('CD'), arms_lengths('DE'), arms_lengths('CE'))/arms_lengths('CE');
 
     vectors('CG') = sqrt(arms_lengths('CD')^2-height^2)*vectors('CE')/norm(vectors('CE'));
+    if sqrt(arms_lengths('DE')^2-height^2) >= norm(points('E')-points('C'))
+        vectors('CG') = -vectors('CG');
+    end 
+    
     vectors('GD') = height*vectors('cross2')/norm(vectors('cross2'));
-    vectors('CD') = vectors('CG')+vectors('GD');
+    vectors('CD') = vectors('CG')+vectors('GD'); %TODO: Account for cases where G lies outside CE
 
     points('D') = points('C')+vectors('CD');
 
@@ -58,7 +73,7 @@ for j = 1:10000
     angles('D') = pi-cosine_law_angle(arms_lengths('CD'), arms_lengths('DE'), norm(points('E')-points('C')));
     angles('E') = pi-cosine_law_angle(arms_lengths('DE'), arms_lengths('EF'), norm(points('F')-points('D')));
     temp = points('D');
-    angles('T') = pi-atan((temp(3)-z0)/temp(2));
+    angles('T') = atan((temp(3)-z0)/temp(2));
 
     x_val = [];
     y_val = [];
@@ -79,32 +94,37 @@ for j = 1:10000
         end
 
         if(i ~= 1 && i ~= 2 && i ~= 6)
-            text(val{i}(1)+0.5, val{i}(2)+0.5, val{i}(3)+0.5, num2str(angles(k{i})));
+            text(val{i}(1)+0.5, val{i}(2)+0.5, val{i}(3)+0.5, num2str(angles(k{i})), 'Color', 'r');
         end 
 
         if (i == 3)
             txt = ["Plane rotation", num2str(angles('T'))];
-            text(val{i}(1)-1, val{i}(2)-1, val{i}(3)-1, txt);
+            text(val{i}(1)-1, val{i}(2)-1, val{i}(3)-1, txt, 'Color', 'r');
+        end 
+        
+        if(abs(arms_lengths('EF')-norm(points('F')-points('E'))) > 0.1)
+            error("EF OFF");
+        end 
+        
+        if(abs(norm(points('E')-points('D'))-arms_lengths('DE')) > 0.1)
+            error("DE OFF");
         end 
         
         hold on;
     end
 
     plot3(x_val, y_val, z_val); %Draw
-    axis([0, 12, -7, 7, 0, 12]);
+    axis([xmin, xmax, ymin, ymax, zmin, zmax]);
+    
+    t = linspace(1, 100);
+    xcircle = xdist.*ones(size(t));
+    ycircle = r*cos(t);
+    zcircle = r*sin(t)+zdist;
+    line(xcircle, ycircle, zcircle);
     
     xlabel('X');
     ylabel('Y');
     zlabel('Z');
-
-
-    
-    r = 2;
-    t = linspace(1, 100);
-    xcircle = 9.*ones(size(t));
-    ycircle = r*cos(t);
-    zcircle = r*sin(t)+5;
-    line(xcircle, ycircle, zcircle);
     
     drawnow;
     hold off;
